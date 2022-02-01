c8cd8d65100926693d04674726dfcf3528d556b9
Charles Cox <charles.h.cox@gmail.com>
2022-01-31 18:29:52 -0600

    Update getrandom.sh

--- getrandom.sh
+++ getrandom.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# if a TPM2 device is available
+tpm2_getrandom 32 | basenc --base64 > ~/random.dat
+
+# if a TPM2 device is not available
 # get data from the RNG
-dd if=/dev/random bs=32 count=1 of=/home/charlesc/random.dat status=progress
+# dd if=/dev/random bs=32 count=1 of=/home/charlesc/random.dat status=progress
